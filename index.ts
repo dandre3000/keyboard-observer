4f61a0e7cd7cd767e1ec3d8b89654eb054da20ab
dandre3000 <deaundre.payne@gmail.com>
2026-04-13 11:55:36 -0400

    Add KeyboardInput.#listener and KeyboardInput.#symbol

--- index.ts
+++ index.ts
@@ -1,26 +1,42 @@
 type KeyCode = KeyboardEvent['code'] | KeyboardEvent['key']
 
-interface KeyboardInputData {
+interface KeyboardInputData extends EventListenerObject {
     symbol: symbol
     eventTarget: EventTarget
     buttons: Set<KeyCode>
+    handleEvent: (this: KeyboardInputData) => void
 }
 
-const KeyboardInputSymbol = Symbol()
-
 export class KeyboardInput {
+    static #symbol = Symbol()
+
     #data: KeyboardInputData
 
-    constructor (eventTarget: EventTarget) {
-        this.#data = {
-            symbol: KeyboardInputSymbol,
-            eventTarget,
-            buttons: new Set
+    static #listener (this: KeyboardInputData, event: KeyboardEvent) {
+        if (event.type === 'keyup') {
+            this.buttons.delete(event.code)
+            if (event.code !== event.key) this.buttons.delete(event.key)
+        } else {
+            this.buttons.add(event.code)
+            if (event.code !== event.key) this.buttons.add(event.key)
         }
     }
 
+    constructor (eventTarget: EventTarget) {
+        this.#data = {
+            symbol: KeyboardInput.#symbol,
+            eventTarget,
+            buttons: new Set,
+            handleEvent: KeyboardInput.#listener
+        }
+
+        eventTarget.addEventListener('keydown', this.#data, true)
+        eventTarget.addEventListener('keypress', this.#data, true)
+        eventTarget.addEventListener('keyup', this.#data, true)
+    }
+
     areButtonsPressed <T extends KeyCode[]>(...keyCodes: T): T['length'] extends 1 ? boolean : boolean[] {
-        if (this.#data?.symbol !== KeyboardInputSymbol)
+        if (this.#data?.symbol !== KeyboardInput.#symbol)
             throw TypeError(`this (${Object.prototype.toString.call(this)}) is not a KeyboardInput instance`)
 
         if (keyCodes.length === 1) return this.#data.buttons.has(String(keyCodes[0])) as any
@@ -35,7 +51,7 @@ export class KeyboardInput {
     }
 
     getPressedButtonSet () {
-        if (this.#data?.symbol !== KeyboardInputSymbol)
+        if (this.#data?.symbol !== KeyboardInput.#symbol)
             throw new TypeError(`this (${Object.prototype.toString.call(this)}) is not a KeyboardInput instance`)
 
         return new Set(this.#data.buttons)
